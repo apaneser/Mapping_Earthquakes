c39dac0bd745d5ae5cbc27ece5ec37301ce7bfc9
apaneser <56094764+apaneser@users.noreply.github.com>
2021-09-18 00:42:30 -0700

    mapping multiple points on a map

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,27 @@
+let cityData = cities;
+
+// Add console.log to see if the code is working
+console.log("working");
+
+// Create the map object with a center and zoom level
+let map = L.map('mapid').setView([40.7, -94.5], 4)
+
+// We create the tile layer that will be in the background of our map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//add our "greymap" tile layer tot he map
+streets.addTo(map);
+
+cityData.forEach(function(city) {
+    console.log(city);
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        lineWeight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map);
+});
